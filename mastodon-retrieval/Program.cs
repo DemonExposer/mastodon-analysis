aa135180a57f8c07a55cd563f8ba82abca8800ba
DemonExposer <56930597+DemonExposer@users.noreply.github.com>
2023-12-02 14:38:03 +0100

    Get data from Mastodon

--- mastodon-retrieval/Program.cs
+++ mastodon-retrieval/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 
-Console.WriteLine("Hello, World!");
+namespace mastodon_retrieval;
+
+public class Program {
+	public static async Task Main(string[] args) {
+		HttpClient client = new () {
+			BaseAddress = new Uri("https://sigmoid.social"),
+		};
+		
+		HttpResponseMessage result = await client.GetAsync("/api/v1/directory?local=true&limit=5&offset=0");
+		int remainingRequests = int.Parse(result.Headers.GetValues("X-RateLimit-Remaining").First());
+		string resetDate = result.Headers.GetValues("X-RateLimit-Reset").First();
+		Console.WriteLine(remainingRequests);
+		Console.WriteLine(resetDate);
+		JsonArray accounts = JsonNode.Parse(await result.Content.ReadAsStringAsync())!.AsArray();
+		Console.WriteLine(JsonSerializer.Serialize(accounts, new JsonSerializerOptions {WriteIndented = true}));
+	}
+}
