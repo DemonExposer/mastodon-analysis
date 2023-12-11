e67e2c1676f6323802388ca304de52aa9938ac84
DemonExposer <kbdmns@gmail.com>
2023-12-11 12:30:21 +0100

    More data and merging of data

---
+++ analysis/merge.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+var res = [];
+fs.readdirSync(".").forEach(file => {
+	if (file.endsWith(".json")) {
+		JSON.parse(fs.readFileSync(file, "utf8")).forEach(elem => res.push(elem));
+	}
+});
+fs.writeFileSync("all_accounts.json", JSON.stringify(res, null, 4));
--- mastodon-retrieval/Program.cs
+++ mastodon-retrieval/Program.cs
@@ -4,32 +4,40 @@ namespace mastodon_retrieval;
 
 public class Program {
 	public static async Task Main(string[] args) {
-		HttpClient client = new () {
-			BaseAddress = new Uri("https://sigmoid.social"),
-		};
+		string[] domains = await File.ReadAllLinesAsync("domains.txt");
+		
+		foreach (string domain in domains) try {
+			HttpClient client = new() {
+				BaseAddress = new Uri($"https://{domain}")
+			};
 
-		JArray totalAccounts = new JArray();
-		for (int offset = 0;; offset += 80) {
-			HttpResponseMessage result = await client.GetAsync($"/api/v1/directory?local=true&limit=80&offset={offset}");
-			int remainingRequests = int.Parse(result.Headers.GetValues("X-RateLimit-Remaining").First());
-			string resetDate = result.Headers.GetValues("X-RateLimit-Reset").First();
-			Console.WriteLine(remainingRequests);
-			Console.WriteLine(resetDate);
-			DateTime properDateTime = DateTime.Parse(resetDate);
-			
-			JArray accounts = JArray.Parse(await result.Content.ReadAsStringAsync());
-			
-			foreach (JToken? t in accounts)
-				totalAccounts.Add(t.Value<JObject>()!);
+			JArray totalAccounts = new JArray();
+			for (int offset = 0;; offset += 80) {
+				HttpResponseMessage result =
+					await client.GetAsync($"/api/v1/directory?local=true&limit=80&offset={offset}");
+				int remainingRequests = int.Parse(result.Headers.GetValues("X-RateLimit-Remaining").First());
+				string resetDate = result.Headers.GetValues("X-RateLimit-Reset").First();
+				Console.WriteLine(remainingRequests);
+				Console.WriteLine(resetDate);
+				DateTime properDateTime = DateTime.Parse(resetDate);
 
-			if (remainingRequests == 0) {
-				TimeSpan waitingTime = properDateTime.Subtract(DateTime.Now).Add(new TimeSpan(0, 0, 5));
-				Thread.Sleep(waitingTime);
+				JArray accounts = JArray.Parse(await result.Content.ReadAsStringAsync());
+
+				foreach (JToken? t in accounts)
+					totalAccounts.Add(t.Value<JObject>()!);
+
+				if (remainingRequests == 0) {
+					TimeSpan waitingTime = properDateTime.Subtract(DateTime.Now).Add(new TimeSpan(0, 0, 5));
+					Thread.Sleep(waitingTime);
+				}
+
+				if (accounts.Count < 80)
+					break;
 			}
-			
-			if (accounts.Count < 80)
-				break;
+
+			await File.WriteAllTextAsync($"accounts_{domain}.json", totalAccounts.ToString());
+		} catch (Exception e) {
+			Console.WriteLine("pech man");
 		}
-		await File.WriteAllTextAsync("accounts.json", totalAccounts.ToString());
 	}
 }
