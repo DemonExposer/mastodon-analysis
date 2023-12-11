f6dc2e0268ae222318cfe5f6288aad3eed34374e
DemonExposer <kbdmns@gmail.com>
2023-12-11 12:41:45 +0100

    Put the results into monthly buckets

---
+++ analysis/sort.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+var res = {};
+JSON.parse(fs.readFileSync("all_accounts.json", "utf8")).forEach(account => {
+	const date = account.created_at.substring(0, 7);
+	if (res[date] === undefined) {
+		res[date] = 1;
+	} else {
+		res[date]++;
+	}
+});
+fs.writeFileSync("months.json", JSON.stringify(res, null, 4));
